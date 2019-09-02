effd42e538a75ec40f8f95ab95f737da0c5600f2
foggybrains <dustin.keyes@gmail.com>
2019-09-01 20:36:51 -0600

    more styling!

--- animations.js
+++ animations.js
@@ -1,21 +1,33 @@
+// ON STARTUP
 // header
-TweenLite.from('#user-log', 5, { opacity: 0 })
+TweenLite.from('#user-log', 5, { opacity: 0, backgroundColor: 'white'})
 TweenLite.from('#logo', 5, { x:'90vw' })
 
 // video 
 TweenLite.fromTo('video', 2.5, {
     borderRadius: '75%', 
     scale: 0,
-    delay: 0.5
+    // delay: 0.5
 }, {
     borderRadius: '0.5rem', 
     scale: 1,
-    delay: 0.5
+    // delay: 0.5
     
 })
 
 // li
-TweenMax.staggerFrom('li', 0.65, { opacity:0, x:'-25vw', y:'50vh', rotation: 90, delay:3 }, 0.2)
+TweenMax.staggerFrom('li', 0.5, { opacity:0, x:'-10vw', y:'50vh', rotation: 90, delay:2.5 }, 0.2)
 
 // form
-TweenMax.staggerFrom('form', 0.65, { opacity:0, x:'25vw', y:'50vh', rotation: -90, delay:4.5 }, 0.5)
+TweenMax.staggerFrom('form', 0.5, { opacity:0, x:'20vw', y:'50vh', rotation: -90, delay:4 }, 0.3)
+
+// EVENT LISTENERS
+const logo = document.querySelector('#logo')
+
+logo.addEventListener('click', (event) => {
+    TweenLite.to('#logo', 2.5, { 
+        backgroundColor: 'white',
+        borderRadius: '0.35rem', 
+        boxShadow: '0 0 7px 7px #007da3 inset, 0 0 10px 10px white'
+    })
+})
--- app.js
+++ app.js
@@ -1,6 +1,47 @@
-const Lis = document.querySelectorAll('li')
-const curriculum = document.querySelector('#curriculum')
+let Lis = document.querySelectorAll('li')
+const $curriculum = document.querySelector('#curriculum')
+const instructorSelect = document.querySelector('#instructor-select')
+const instructor = document.querySelector('#instructor-name')
+const curriculumButton = document.querySelector('#curriculum-button')
+const $curriculumList = document.querySelector('.curriculum-list')
+const $materials = document.querySelector('.materials')
 
 Array.from(Lis).map(li => li.addEventListener('click', event => {
-    curriculum.innerText = event.target.innerText
-    }))
+    $curriculum.innerText = event.target.innerText
+}))
+
+instructorSelect.addEventListener('change', (event)=> {
+    instructor.innerText = `Instructor's Name: ${event.target.value}`
+})
+
+const curriculum = [
+    'Basic English Elocution',
+    'Airport Terminology',
+    'Aircraft Terminology',
+    'Pilot Terminology',
+    'Weather Terminology',
+    'VFR Terminology',
+    'IFR Terminology',
+    'Sample Test'
+]
+
+curriculumButton.addEventListener('click', (event) => {
+    if (curriculumButton.innerText === 'Hide Curriculum') {
+        console.log('hit hide curriculum')
+        curriculumButton.innerText = 'Show Curriculum'
+        while ($curriculumList.firstChild) {
+            $curriculumList.removeChild($curriculumList.firstChild)
+        }
+    } else {
+        curriculumButton.innerText = 'Hide Curriculum'
+        curriculum.map(module => {
+            const ul = document.querySelector('.curriculum-list')
+            const li = document.createElement('li')
+            li.className = 'new-item'
+            li.innerText = module
+            ul.appendChild(li)
+        })
+        // TweenLite.to('.materials', 0.4, { expand_content, opacity:0, x:'-10vw', y:'50vh', rotation: 90 }, 0.1)
+        TweenMax.staggerFrom('li', 0.4, { opacity:0, x:'-10vw', y:'50vh', rotation: 90 }, 0.1)
+    }
+})
