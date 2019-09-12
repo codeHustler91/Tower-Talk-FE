71c64d7c92ecd70a9ae76ed14c4e7c91add9116f
foggybrains <dustin.keyes@gmail.com>
2019-09-12 12:08:29 -0600

    bugs

--- videoStream.js
+++ videoStream.js
@@ -1,33 +1,31 @@
-const mediaSelect = document.querySelector('#media-select')
+// const mediaSelect = document.querySelector('#media-select')
 let media = {
-  video: false,
+  video: true,
   audio: false
 }
 
-mediaSelect.addEventListener('change', (event) => {
-  const chosen = event.target.value
-  if (chosen === 'Video & Audio') {
-    console.log('AV')
-    media.video = true;
-    media.audio = true;
-  } else if (chosen === 'Video Only') {
-    console.log('Video only')
-    media.video = true;
-    media.audio = false;
-  } else if (chosen === 'Audio Only') {
-    console.log('Audio')
-    media.video = false;
-    media.audio = true;
-  } else { 
-    console.log('else')
-    media.video = false;
-    media.audio = false;
-  }
-  navigator.getUserMedia(media, gotMedia, function (err) {console.error(err)})
-})
-
-function gotMedia (stream) {
-    console.log('video', stream)
+// mediaSelect.addEventListener('change', (event) => {
+//   const chosen = event.target.value
+//   if (chosen === 'Video & Audio') {
+//     console.log('AV')
+//     media.video = true;
+//     media.audio = true;
+//   } else if (chosen === 'Video Only') {
+//     console.log('Video only')
+//     media.video = true;
+//     media.audio = false;
+//   } else if (chosen === 'Audio Only') {
+//     console.log('Audio')
+//     media.video = false;
+//     media.audio = true;
+//   } else { 
+//     console.log('else')
+//     media.video = false;
+//     media.audio = false;
+//   }
+// })
+navigator.getUserMedia(media, function (stream) {
+  console.log('video', stream)
   let peer = new SimplePeer({ 
     initiator: location.hash === '#init',
     trickle: false,
@@ -49,19 +47,6 @@ function gotMedia (stream) {
     video.src = window.URL.createObjectURL(stream)
     video.play()
   })
-}
-
-
-  // peer2.on('signal', data => {
-  //   peer1.signal(data)
-  // })
-
-  // peer2.on('stream', stream => {
-  //   var video = document.querySelector('video')
-
-  //   if ('srcObject' in video) {
-  //     video.srcObject = stream
-  //   } else {
-  //     console.log('older browser?')
-  //     // video.src = window.URL.createObjectURL(stream) // for older browsers
-  //   }
+}, function (err) {
+  console.error(err)
+})
